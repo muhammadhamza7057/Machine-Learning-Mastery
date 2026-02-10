b20f4c54b6e384c2594f46b834aed20cfe335039
muhammadhamza7057 <muhammadhamza7057@gmail.com>
2026-02-10 11:08:25 +0500

    Initial commit: Add Python learning structure and notes. The basic syntax and varibales i have learned.

---
+++ hy.py
@@ -0,0 +1,32 @@
+import sys
+print(sys.version)
+print("This is Muhammad Hamza Software Engineer")
+if 5 > 3:
+    print("5 is greater than 3")
+else :
+    print("5 is not greater than 3")
+
+    #This is the muhammad hamza
+    #This is the software engineer
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print("Hello World!")
+print("I will print on the same line.")
+
+x = 5
+y = "Hamza"
+print("This is the number : " + str(x))
+print("This is the name : " + y)
+
+
+print(type(x))
+print(type(y))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
