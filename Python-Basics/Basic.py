231eb3b3a6854cf55604db9bd018a58820c64db3
muhammadhamza7057 <muhammadhamza7057@gmail.com>
2026-03-15 23:13:03 +0500

    Learmed the if else and elif conditions and how we can used these in various senirio.

--- Python-Basics/Basic.py
+++ Python-Basics/Basic.py
@@ -316,3 +316,28 @@ if b > a:
   is_logged_in = True
 if is_logged_in:
   print("Welcome back!")
+  
+  a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+  
+  
+  age = 25
+
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
+  
+y = 200
+g = 33
+h = 500
+if y > g and h > y:
+  print("Both conditions are True")
